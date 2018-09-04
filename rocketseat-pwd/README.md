bf1be73c284bb55dc8362b6db11129153085a856
Karlos Kelvin <kelvin.santos@ufca.edu.br>
2018-09-04 17:08:17 -0300

    Projeto estudo de PWD com ReactJS

---
+++ rocketseat-pwd/README.md
@@ -0,0 +1,1 @@
+Projeto de estudo de PWD com ReactJS
---
+++ rocketseat-pwd/src/App.js
@@ -0,0 +1,53 @@
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
+
+import { Header, Repositories } from './style';
+
+export default class App extends Component {
+  state = {
+    newRepoInput: '',
+    repositories: [],
+  }
+
+  addRepository = async () => {
+    if (!this.state.newRepoInput) return;
+
+    const response = await axios.get(`https://api.github.com/repos/${this.state.newRepoInput}`);
+
+    this.setState({
+      newRepoInput: '',
+      repositories: [ ...this.state.repositories, response.data],
+    });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Header>
+          <input
+            placeholder="Adicionar repositório"
+            onChange={e => this.setState({ newRepoInput: e.target.value })}
+            value={this.state.newRepoInput}
+          />
+
+          <button onClick={this.addRepository}>Adicionar</button>
+        </Header>
+
+        <Repositories>
+          {
+            this.state.repositories.map(repository => (
+              <li key={repository.id}>
+                <img src={repository.owner.avatar_url}/>
+                <div>
+                  <strong>{repository.name}</strong>
+                  <p>{repository.description}</p>
+                  <a href={repository.html_url}>Acessar</a>
+                </div>
+              </li>
+            ))
+          }
+        </Repositories>
+      </Fragment>
+    );
+  }
+}
